67a54984e1abeea5af86520a1dfb8bb09e981f75
Thiago Rafael <thiago.rafael@ufpi.edu.br>
2026-05-13 23:23:36 -0300

    correcao no teste merge

--- testes/teste_merge.py
+++ testes/teste_merge.py
@@ -2,8 +2,8 @@ from algoritmos.merge_sort import merge_sort
 
 vetor = [5, 2, 4, 6, 1, 3]
 
-ordenado, comparacoes = merge_sort(vetor)
+ordenado, comparacoes = merge_sort(vetor.copy())
 
-print("Vetor original:", [5, 2, 4, 6, 1, 3])
+print("Vetor original:", vetor)
 print("Vetor ordenado:", ordenado)
 print("Comparações:", comparacoes)
