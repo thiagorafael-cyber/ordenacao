17c068b65881b301bc18feefe5bab344ed53264b
Thiago Rafael <thiago.rafael@ufpi.edu.br>
2026-05-13 23:22:01 -0300

    arquivo do merge e teste

--- algoritmos/merge_sort.py
+++ algoritmos/merge_sort.py
@@ -1,56 +1,52 @@
-def merge(vetor, esquerda, meio, direita):
+def merge(vetor, primeiro, meio, ultimo):
     comparacoes = 0
 
-    n1 = meio - esquerda + 1
-    n2 = direita - meio
+    n1 = meio - primeiro + 1
+    n2 = ultimo - meio
 
     L = [0] * n1
     R = [0] * n2
 
     for i in range(n1):
-        L[i] = vetor[esquerda + i]
+        L[i] = vetor[primeiro + i]
 
     for j in range(n2):
         R[j] = vetor[meio + 1 + j]
 
     i = 0
     j = 0
-    k = esquerda
 
-    while i < n1 and j < n2:
-        comparacoes += 1
-
-        if L[i] <= R[j]:
-            vetor[k] = L[i]
-            i = i + 1
-        else:
+    for k in range(primeiro, ultimo + 1):
+        if i >= n1 and j < n2:
             vetor[k] = R[j]
             j = j + 1
 
-        k = k + 1
+        elif j >= n2 and i < n1:
+            vetor[k] = L[i]
+            i = i + 1
 
-    while i < n1:
-        vetor[k] = L[i]
-        i = i + 1
-        k = k + 1
+        else:
+            comparacoes += 1
 
-    while j < n2:
-        vetor[k] = R[j]
-        j = j + 1
-        k = k + 1
+            if L[i] <= R[j]:
+                vetor[k] = L[i]
+                i = i + 1
+            else:
+                vetor[k] = R[j]
+                j = j + 1
 
     return comparacoes
 
 
-def merge_sort_recursivo(vetor, esquerda, direita):
+def merge_sort_recursivo(vetor, primeiro, ultimo):
     comparacoes = 0
 
-    if esquerda < direita:
-        meio = (esquerda + direita) // 2
+    if primeiro < ultimo:
+        meio = (primeiro + ultimo) // 2
 
-        comparacoes += merge_sort_recursivo(vetor, esquerda, meio)
-        comparacoes += merge_sort_recursivo(vetor, meio + 1, direita)
-        comparacoes += merge(vetor, esquerda, meio, direita)
+        comparacoes += merge_sort_recursivo(vetor, primeiro, meio)
+        comparacoes += merge_sort_recursivo(vetor, meio + 1, ultimo)
+        comparacoes += merge(vetor, primeiro, meio, ultimo)
 
     return comparacoes
 
--- testes/teste_merge.py
+++ testes/teste_merge.py
@@ -4,5 +4,6 @@ vetor = [5, 2, 4, 6, 1, 3]
 
 ordenado, comparacoes = merge_sort(vetor)
 
+print("Vetor original:", [5, 2, 4, 6, 1, 3])
 print("Vetor ordenado:", ordenado)
 print("Comparações:", comparacoes)
